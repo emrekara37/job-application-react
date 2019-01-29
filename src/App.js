ad9d7bf60f31b1f3cb77ab507586f7b2e7a6b0f0
Emre KARA <emre.kara5@ogr.sakarya.edu.tr>
2019-01-29 20:45:50 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Reacts
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "EmreKARa"
+        };
+
+    }
+
+    componentWillMount() {
+        console.log("componentWillMount");
+        setTimeout(() => {
+            this.setState({name: "Emre"})
+        }, 2000)
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount");
+    }
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        
+    }
+
+    render() {
+        console.log("Render");
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        {this.state.name}
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn Reacts
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
